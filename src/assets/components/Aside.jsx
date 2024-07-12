dcc514b96112176328ba6e3ad9a3d8ee7bfec4d8
kimbohy <lovatina123@gmail.com>
2024-07-12 09:38:57 +0300

    beginning aside functionality

--- src/assets/components/Aside.jsx
+++ src/assets/components/Aside.jsx
@@ -1,4 +1,5 @@
 function Aside() {
+  localStorage.setItem("page", "dashboard");
   return (
     <aside>
       <div id="profile">
@@ -6,15 +7,15 @@ function Aside() {
         <p id="logName">Nom</p>
       </div>
       <ul id="menu">
-        <li id="dashboardSection">
+        <li id="dashboardSection" onClick={localStorage.setItem("page", "dashboard")}>
           <img src="/src/assets/img/app.png" alt="" />
           <p>Dashboard</p>
         </li>
-        <li id="userSection">
+        <li id="userSection" onClick={localStorage.setItem("page", "users")}>
           <img src="/src/assets/img/user.png" alt="" />
           <p>Users</p>
         </li>
-        <li id="groupSection">
+        <li id="groupSection" onClick={localStorage.setItem("page", "group")}>
           <img src="/src/assets/img/people.png" alt="" />
           <p>Groups</p>
         </li>
--- src/assets/components/Content.jsx
+++ src/assets/components/Content.jsx
@@ -1,10 +1,14 @@
 import UserList from "./UserList";
+import { useState } from "react";
 
 function Content() {
+  const [page, setPage] = useState("dashboard");
   return (
     <div>
-      <h1>Content</h1>
-      <UserList />
+      {localStorage.getItem("page") === "dashboard" && <UserList />}
+      {localStorage.getItem("page") === "users" && <h1>Users</h1>}
+      {localStorage.getItem("page") === "group" && <h1>Groups</h1>}
+      {!localStorage.getItem("page") && <h1>Dashboard</h1>}
     </div>
   );
 }
